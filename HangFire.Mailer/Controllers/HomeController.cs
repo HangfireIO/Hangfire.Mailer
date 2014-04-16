d2b87b2ab750026d9a721ffdd767c02bbccef62d
Sergey Odinokov <odinserj@gmail.com>
2014-04-17 01:05:20 +0400

    Added HangFire

--- HangFire.Mailer/Controllers/HomeController.cs
+++ HangFire.Mailer/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using HangFire.Mailer.Models;
+using Postal;
 
 namespace HangFire.Mailer.Controllers
 {
@@ -23,17 +26,35 @@ namespace HangFire.Mailer.Controllers
                 _db.Comments.Add(model);
                 _db.SaveChanges();
 
-                var email = new NewCommentEmail
-                {
-                    To = "yourmail@example.com",
-                    UserName = model.UserName,
-                    Comment = model.Text
-                };
-
-                email.Send();
+                BackgroundJob.Enqueue(() => NotifyNewComment(model.Id));
             }
 
             return RedirectToAction("Index");
+        }
+
+        public static void NotifyNewComment(int commentId)
+        {
+            // Prepare Postal classes to work outside of ASP.NET request
+            var viewsPath = Path.GetFullPath(HostingEnvironment.MapPath(@"~/Views/Emails"));
+            var engines = new ViewEngineCollection();
+            engines.Add(new FileSystemRazorViewEngine(viewsPath));
+
+            var emailService = new EmailService(engines);
+
+            // Get comment and send a notification.
+            using (var db = new MailerDbContext())
+            {
+                var comment = db.Comments.Find(commentId);
+
+                var email = new NewCommentEmail
+                {
+                    To = "yourmail@example.com",
+                    UserName = comment.UserName,
+                    Comment = comment.Text
+                };
+
+                emailService.Send(email);
+            }
         }
 
         protected override void Dispose(bool disposing)
