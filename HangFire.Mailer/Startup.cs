6d155d66b5b0c88b2e8d96c85f2540a85912d312
Sergey Odinokov <odinserj@gmail.com>
2014-06-15 22:05:49 +0400

    Update HangFire to 1.0.0-alpha1

---
+++ HangFire.Mailer/Startup.cs
@@ -0,0 +1,20 @@
+﻿using HangFire.SqlServer;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HangFire.Mailer.Startup))]
+
+namespace HangFire.Mailer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHangFire(config =>
+            {
+                config.UseSqlServerStorage("MailerDb");
+                config.UseServer();
+            });
+        }
+    }
+}
